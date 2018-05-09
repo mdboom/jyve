238c485823059d1f3b548020fa3d5340079ee575
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-05-09 19:55:59 -0400

    lint pyodide

--- packages/jyve-pyodide-unsafe-extension/src/index.ts
+++ packages/jyve-pyodide-unsafe-extension/src/index.ts
@@ -1,6 +1,8 @@
 import {JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
 import {IJyve} from '@deathbeds/jyve';
-const pkg = (require('../package.json') as any);
+
+// tslint:disable-next-line
+const pkg = require('../package.json') as any;
 
 const id = '@deathbeds/jyve-pyodide-unsafe-extension';
 
@@ -10,14 +12,13 @@ const extension: JupyterLabPlugin<void> = {
   id,
   autoStart: true,
   requires: [IJyve],
-  activate: (
-    app: JupyterLab, jyve: IJyve
-  ) => {
+  activate: (app: JupyterLab, jyve: IJyve) => {
     jyve.register({
       kernelSpec: pkg.jyve.kernelspec,
-      newKernel: import('@deathbeds/jyve-pyodide-unsafe') as any
+      // tslint:disable-next-line
+      newKernel: import('@deathbeds/jyve-pyodide-unsafe') as any,
     });
-  }
+  },
 };
 
 export default extension;
